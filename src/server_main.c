82308faccc61e0fdad42603e1740ceb4a538e92f
Anssi Mäkinen <amakinen@student.hive.fi>
2024-10-29 14:45:32 +0200

    Abort previous transmission if detecting a different client
    This doesn't support simultaneous clients, but ensures the server
    remains usable if a transmission gets stuck for whatever reason.

--- src/server_main.c
+++ src/server_main.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:39:50 by amakinen          #+#    #+#             */
-/*   Updated: 2024/10/29 14:20:11 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:43:14 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,51 @@
 #include "signals.h"
 #include "util.h"
 
+static void	check_sender(pid_t *client, pid_t sender, t_receive_state *state)
+{
+	if (*client != 0 && sender != *client)
+	{
+		receive_reset(state);
+		signals_send_bit(*client, 1);
+	}
+	*client = sender;
+}
+
+static void	try_receive_bit(pid_t *client, bool bit, t_receive_state *state)
+{
+	if (!receive_add_bit(state, bit))
+	{
+		receive_reset(state);
+		signals_send_bit(*client, 1);
+		*client = 0;
+		return ;
+	}
+	if (receive_done(state))
+	{
+		write(STDOUT_FILENO, state->buf, state->len);
+		receive_reset(state);
+		signals_send_bit(*client, 0);
+		*client = 0;
+		return ;
+	}
+	signals_send_bit(*client, 0);
+}
+
 int	main(void)
 {
 	t_signal_data	sig_data;
 	t_receive_state	receive_state;
+	pid_t			client;
 
 	util_write_int(STDOUT_FILENO, getpid());
 	write(STDOUT_FILENO, "\n", 1);
 	receive_init(&receive_state);
 	signals_set_handler();
+	client = 0;
 	while (1)
 	{
 		sig_data = signals_wait_for_data();
-		if (!receive_add_bit(&receive_state, sig_data.bit))
-		{
-			receive_reset(&receive_state);
-			signals_send_bit(sig_data.sender, 1);
-			continue ;
-		}
-		if (receive_done(&receive_state))
-		{
-			write(STDOUT_FILENO, receive_state.buf, receive_state.len);
-			receive_reset(&receive_state);
-		}
-		signals_send_bit(sig_data.sender, 0);
+		check_sender(&client, sig_data.sender, &receive_state);
+		try_receive_bit(&client, sig_data.bit, &receive_state);
 	}
 }
